d0d74280f4f9e6091825c7d62b43d977026a83a2
stappost <emanuele.scarpantoni@gmail.com>
2024-02-21 15:04:00 +0100

    seed projects table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+
+use App\Models\Project;
+
 class ProjectSeeder extends Seeder
 {
     /**
@@ -12,8 +16,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i<10; $i++){
+            $new_project = new Project();
+            $new_project->name = $faker->words(3, true);
+            $new_project->description = $faker->paragraphs(2, true);
+            $new_project->start_project = $faker->dateTimeBetween('-4 week', '-1 week');
+            $new_project->finish_project = $faker->dateTimeBetween('-1 week', now());
+            $new_project->in_team = '0';
+
+            $new_project->save();
+        }
     }
 }
