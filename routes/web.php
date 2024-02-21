d8be1d12f26fc708c5fe33bb853ac93dd43de4b9
stappost <emanuele.scarpantoni@gmail.com>
2024-02-21 15:09:31 +0100

    fix link controller in web and dashboard link in layout/app

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\DashboardController as DashboardController;
 
 /*
 |--------------------------------------------------------------------------
