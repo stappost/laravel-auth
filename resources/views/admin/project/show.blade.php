f160fdd60a41bb8c3b054c6df08425d18c67791a
stappost <emanuele.scarpantoni@gmail.com>
2024-02-26 14:10:28 +0100

    add logo in show

--- resources/views/admin/project/show.blade.php
+++ resources/views/admin/project/show.blade.php
@@ -5,6 +5,13 @@
         <div class="row">
             <div class="col-12 text-center mt-5">
                 <h2>{{ $project->name }}</h2>
+                <div class="logo">
+                    @if ($project->logo != null)
+                        <img src="{{ asset('/storage/' . $project->logo) }}" alt="{{ $project->name }}">
+                    @else
+                        <img src="{{ asset('/placeholder/logo.jpeg') }}" alt="{{ $project->name }}">
+                    @endif
+                </div>
                 <div class="my-3">
                     Inizio Progetto: {{ $project->start_project }}
                     <p>{{ $project->finish_project ? 'Fine Progetto: ' . $project->finish_project : '' }}</p>
